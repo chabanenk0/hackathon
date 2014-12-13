73cc4da67694582905375602ee09668cdc712cc5
intensity <intensity.web@gmail.com>
2014-12-13 20:19:25 +0200

    google maps, dinners

--- src/Hackaton/DinningRoomBundle/Entity/Enterprise.php
+++ src/Hackaton/DinningRoomBundle/Entity/Enterprise.php
@@ -3,17 +3,18 @@
 namespace Hackaton\DinningRoomBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Enterprise
  *
  * @ORM\Table(name="enterprises")
- * @ORM\Entity(repositoryClass="Acme\DemoBundle\Entity\EnterpriseRepository")
+ * @ORM\Entity(repositoryClass="Hackaton\DiningRoomBundle\Entity\EnterpriseRepository")
  * @ORM\InheritanceType("SINGLE_TABLE")
  * @ORM\DiscriminatorColumn(name="type", type="integer")
  * @ORM\DiscriminatorMap({"0" = "Enterprise", "1" = "DinningRoom"})
-
+ * @ORM\HasLifecycleCallbacks
  */
 class Enterprise
 {
@@ -36,6 +37,16 @@ class Enterprise
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @Assert\File(maxSize="6000000")
+     */
+    private $file;
+
+    /**
+     * @var $temp
+     */
+    private $temp;
 
     /**
      * @var string
@@ -65,21 +76,21 @@ class Enterprise
      *
      * @ORM\Column(name="latitude", type="float")
      */
-    protected $latitude;
+    public  $latitude;
 
     /**
      * @var double
      *
      * @ORM\Column(name="longitude", type="float")
      */
-    protected $longitude;
+    public  $longitude;
 
     /**
      * @var string
      *
      * @ORM\Column(name="address", type="string", length=50, nullable=true)
      */
-    protected $address;
+    public $address;
 
     /**
      * @var string
@@ -88,6 +99,10 @@ class Enterprise
      * @ORM\Column(name="url", type="string", length=50, nullable=true)
      */
     protected $url;
+
+    public function __construct()
+    {
+    }
 
     /**
      * @return int
@@ -161,5 +176,143 @@ class Enterprise
         $this->url = $url;
     }
 
+    /**
+     * @return float
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
 
+    /**
+     * @param $latitude
+     * @return $this
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param $longitude
+     * @return $this
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param $address
+     * @return $this
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getAbsolutePath()
+    {
+        return null === $this->photo ? null : $this->getUploadDir() . '/' . $this->photo;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->photo ? null : $this->getUploadDir() . '/' . $this->photo;
+    }
+
+    protected function getUploadRootDir()
+    {
+        return __DIR__ . '/../../../../web/' . $this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        return 'uploads/diners';
+    }
+
+    /**
+     * Get file.
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * Sets file.
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+        if (isset($this->photo)) {
+            $this->temp = $this->photo;
+            $this->photo = null;
+        } else {
+            $this->photo = 'initial';
+        }
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        if (null !== $this->getFile()) {
+            $filename = sha1(uniqid(mt_rand(), true));
+            $this->photo = $filename . '.' . $this->getFile()->guessExtension();
+        }
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        if (null === $this->getFile()) {
+            return;
+        }
+        $this->getFile()->move($this->getUploadRootDir(), $this->photo);
+        if (isset($this->temp)) {
+            unlink($this->getUploadRootDir() . '/' . $this->temp);
+            $this->temp = null;
+        }
+        $this->file = null;
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        if ($file = $this->getAbsolutePath()) {
+            unlink($file);
+        }
+    }
 }
